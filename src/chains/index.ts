6883b2b556723f93b04cf3b40fc6e9cf789fab9c
Renfeng Shi <renfeng@keyst.one>
2023-04-14 16:52:53 +0800

    refact: export chains

---
+++ src/chains/index.ts
@@ -0,0 +1,5 @@
+export * from './bitcoin'
+export * from './cosmos'
+export * from './ethereum'
+export * from './solana'
+export * from './tron'
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,13 @@
 import * as utils from './utils'
-import { KeystoneBitcoinSDK } from './chains/bitcoin'
-import { KeystoneEthereumSDK } from './chains/ethereum'
-import { KeystoneSolanaSDK } from './chains/solana'
-import { KeystoneCosmosSDK } from './chains/cosmos'
-import { KeystoneTronSDK } from './chains/tron'
 import { KeystoneSDK } from './sdk'
 
 export * from '@ngraveio/bc-ur'
 export * from './types'
+export * from './chains'
 
 export default KeystoneSDK
 
 export {
   KeystoneSDK,
-  KeystoneBitcoinSDK,
-  KeystoneEthereumSDK,
-  KeystoneSolanaSDK,
-  KeystoneCosmosSDK,
-  KeystoneTronSDK,
   utils
 }

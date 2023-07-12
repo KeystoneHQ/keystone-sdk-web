63cc500f6fc06112d70406fdd6570ebb256b3ca2
Renfeng Shi <renfeng@keyst.one>
2023-07-12 17:05:42 +0800

    fix evm

--- src/chains/evm.ts
+++ src/chains/evm.ts
@@ -2,7 +2,6 @@ import { parsePath, toBuffer, toHex, uuidParse, uuidStringify } from '../utils'
 import { URType, type UR } from '../types/ur'
 import { EvmSignRequest, EvmSignature, CryptoKeypath, PathComponent, SignDataType as DataType } from '@keystonehq/bc-ur-registry-evm'
 import { type EvmSignRequestProps } from '../types/props'
-import * as tracker from '../tracker'
 import { type Signature } from '../types'
 
 export class KeystoneEvmSDK {
@@ -14,9 +13,6 @@ export class KeystoneEvmSDK {
     }
     const sig = EvmSignature.fromCBOR(ur.cbor)
     const requestId = uuidStringify(sig.getRequestId())
-    tracker.track('sign', {
-      requestId
-    })
     return {
       requestId,
       signature: toHex(sig.getSignature())
@@ -32,14 +28,13 @@ export class KeystoneEvmSDK {
     origin
   }: EvmSignRequestProps): UR {
     const derivationPath: CryptoKeypath = new CryptoKeypath(parsePath(account.path).map(e => new PathComponent(e)), toBuffer(account.xfp))
-    tracker.config.xfp = account.xfp
     return new EvmSignRequest({
       signData: toBuffer(signData),
       dataType,
       customChainIdentifier,
       derivationPath,
       requestId: uuidParse(requestId),
-      address: account.address !== undefined ? toBuffer(account.address) : undefined,
+      address: account.address !== undefined ? toBuffer(account.address.substring(account.address.startsWith('0x') ? 2 : 0)) : undefined,
       origin
     }).toUR()
   }

83f8976f51a009d24ea14726dc6bac908845acfa
ZhenQian <zhenqian@keyst.one>
2024-08-06 17:39:14 +0800

    feat: update hardwarecall

--- src/wallet/hardwareCall.ts
+++ src/wallet/hardwareCall.ts
@@ -1,37 +1,64 @@
 import {
-  Curve,
-  DerivationAlgorithm,
-  KeyDerivation,
-  QRHardwareCall,
-  KeyDerivationSchema, QRHardwareCallType
+	Curve,
+	DerivationAlgorithm,
+	KeyDerivation,
+	QRHardwareCall,
+	KeyDerivationSchema,
+	QRHardwareCallType,
+	QRHardwareCallVersion,
 } from '@keystonehq/bc-ur-registry'
 import { type UR } from '@ngraveio/bc-ur'
 import { pathToKeypath } from '../utils'
 
-export { Curve, DerivationAlgorithm }
+export { Curve, DerivationAlgorithm, QRHardwareCallVersion }
 
 export interface KeySchema {
-  path: string
-  curve?: Curve
-  algo?: DerivationAlgorithm
+	path: string
+	curve?: Curve
+	algo?: DerivationAlgorithm
+	chainType?: String
 }
 
 export interface KeyDerivationCallArgs {
-  schemas: KeySchema[]
-  origin?: string
+	schemas: KeySchema[]
+	origin?: string
+	version?: QRHardwareCallVersion
 }
 
 export const generateKeyDerivationCall = ({
-  schemas,
-  origin
+	schemas,
+	origin,
+	version,
 }: KeyDerivationCallArgs): UR => {
-  const keyDerivationSchemas = schemas.map(({ path, curve = Curve.secp256k1, algo = DerivationAlgorithm.slip10 }) => {
-    if (curve === Curve.secp256k1 && algo === DerivationAlgorithm.bip32ed25519) {
-      throw new Error('the combination of the given curve and algo not supported')
-    }
-    return new KeyDerivationSchema(pathToKeypath(path), curve, algo)
-  })
-  const keyDerivation = new KeyDerivation(keyDerivationSchemas)
-  const hardwareCall = new QRHardwareCall(QRHardwareCallType.KeyDerivation, keyDerivation, origin)
-  return hardwareCall.toUR()
+	const keyDerivationSchemas = schemas.map(
+		({
+			path,
+			curve = Curve.secp256k1,
+			algo = DerivationAlgorithm.slip10,
+			chainType,
+		}) => {
+			if (
+				curve === Curve.secp256k1 &&
+				algo === DerivationAlgorithm.bip32ed25519
+			) {
+				throw new Error(
+					'the combination of the given curve and algo not supported'
+				)
+			}
+			return new KeyDerivationSchema(
+				pathToKeypath(path),
+				curve,
+				algo,
+				chainType
+			)
+		}
+	)
+	const keyDerivation = new KeyDerivation(keyDerivationSchemas)
+	const hardwareCall = new QRHardwareCall(
+		QRHardwareCallType.KeyDerivation,
+		keyDerivation,
+		origin,
+		version ? QRHardwareCallVersion.V1 : QRHardwareCallVersion.V0
+	)
+	return hardwareCall.toUR()
 }
